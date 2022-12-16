8a73678a8d99795a3a926033b63b127a36424a0d
damir <slim-shady666@mail.ru>
2022-12-16 20:59:11 +0300

    finish

--- home work 1.2/ViewController.swift
+++ home work 1.2/ViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
-
+enum CurrentLight{
+    case red, yellow, green
+}
 class ViewController: UIViewController {
 
     @IBOutlet var redColor: UIView!
@@ -14,6 +16,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var greenColor: UIView!
     @IBOutlet var startButton: UIButton!
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +24,31 @@ class ViewController: UIViewController {
         yllowColor.layer.cornerRadius = 50
         greenColor.layer.cornerRadius = 50
         startButton.layer.cornerRadius = 20
+        redColor.alpha = 0.3
+        yllowColor.alpha = 0.3
+        greenColor.alpha = 0.3
     }
 
     @IBAction func startButtonPressed() {
+        
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenColor.alpha = 0.3
+            redColor.alpha = 1
+            currentLight = .yellow
+            
+        case .yellow:
+            redColor.alpha = 0.3
+            yllowColor.alpha = 1
+            currentLight = .green
+        case .green:
+            yllowColor.alpha = 0.3
+            greenColor.alpha = 1
+            currentLight = .red
+        }
+      
     }
     
 }
